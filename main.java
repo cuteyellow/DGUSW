88954632150483b86e2884aa4ed8a20dbc976703
cuteyellow <bjh@isplus.co.kr>
2024-03-14 19:00:37 +0900

    백종헌 2020112052_2

--- main.java
+++ main.java
@@ -1,42 +1,44 @@
-package practice2ndWeek;
+package main;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
-
 	public static void main(String[] args) {
-		
-		Scanner oUserInput=new Scanner(System.in);
-		
-		String sStudentName="";
-		long lStudentNum=0;
-		
-		while(true) {
+
+		Scanner oUserInput = new Scanner(System.in);
+
+		String sStudentName = "";
+		String sStudentMajor = "";
+		long lStudentNum = 0;
+
+		while (true) {
 			try {
 				System.out.print("학번을 입력하세요>>");
-				lStudentNum=oUserInput.nextLong();
-				
+				lStudentNum = oUserInput.nextLong();
+
 				break;
-			}catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println("잘못된 입력입니다");
 				oUserInput.nextLine();
 			}
 		}
 		oUserInput.nextLine();
-		
+
 		System.out.print("이름을 입력하세요>>");
-		sStudentName=oUserInput.nextLine();
+		sStudentName = oUserInput.nextLine();
 
-		
+		System.out.print("전공을 입력하세요>>");
+		sStudentMajor = oUserInput.nextLine();
+
 		oUserInput.close();
-		
-		System.out.println("학번: "+lStudentNum);
-		System.out.println("이름: "+sStudentName);
 
+		System.out.println("학번: " + lStudentNum);
+		System.out.println("이름: " + sStudentName);
+		System.out.println("전공: " + sStudentMajor);
 
-		
-		
+//with bash
+
 	}
 
 }
